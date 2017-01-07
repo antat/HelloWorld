7a24d6872e89984af32442ec498d36704212752f
y700-15 <y700-15@MyNote>
2017-01-07 23:27:38 +0300

    first commit

--- Hello/src/Helloworld.java
+++ Hello/src/Helloworld.java
@@ -4,6 +4,7 @@ public class Helloworld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
+		System.out.println("1st local commit");
 	}
 
 }
