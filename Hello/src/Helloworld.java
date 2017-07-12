81b8a26d671214bf9bb6b2971f0d051d417b14bd
y700-15 <y700-15@172.20.120.143>
2017-07-13 00:09:54 +0300

    Signed-off-by: y700-15 <y700-15@172.20.120.143>

--- Hello/src/Helloworld.java
+++ Hello/src/Helloworld.java
@@ -1,11 +1,11 @@
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane.SystemMenuBar;
-
 public class Helloworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
 		System.out.println("1st local commit");
+		System.out.println("st local commit");
+		
 		
 	}
 
